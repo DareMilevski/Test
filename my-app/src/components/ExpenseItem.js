5b357bae8ea5f2a3cb5452ea567b34af91302d66
Darko Milevski <milevskidarko1989@outlook.com>
2021-11-02 15:15:22 +0100

    changes

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     const expenseDate = new Date(2021, 2, 28);
     const expenseTitle = 'Dare';
     const expenseAmount = 294.5;
@@ -9,7 +9,8 @@ function ExpenseItem() {
         <div className='expense-item'>
             <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
+                <h2>{props.title}</h2>
+                <h1>{props.id} </h1>
                 <div className='expense-item__price'>$ {expenseAmount}</div>
             </div>
         </div>
